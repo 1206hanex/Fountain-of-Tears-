3dee1f8c057ae72ff19017977fad0ac9cd2aa06d
hans <111059193+1206hanex@users.noreply.github.com>
2023-05-24 17:29:25 +1200

    Update main.js

--- main.js
+++ main.js
@@ -31,25 +31,25 @@ function Init() {
 	controls = new OrbitControls(camera, renderer.domElement)
 	controls.update()
 
-	let light = new THREE.DirectionalLight(0xFFFFFF, 1.0);
-	light.position.set(20, 100, 10);
-	light.target.position.set(0, 0, 0);
-	light.castShadow = true;
-	light.shadow.bias = -0.001;
-	light.shadow.mapSize.width = 2048;
-	light.shadow.mapSize.height = 2048;
-	light.shadow.camera.near = 0.1;
-	light.shadow.camera.far = 500.0;
-	light.shadow.camera.near = 0.5;
-	light.shadow.camera.far = 500.0;
-	light.shadow.camera.left = 100;
-	light.shadow.camera.right = -100;
-	light.shadow.camera.top = 100;
-	light.shadow.camera.bottom = -100;
-	scene.add(light);
+	let light = new THREE.DirectionalLight(0xFFFFFF, 1.0)
+	light.position.set(20, 100, 10)
+	light.target.position.set(0, 0, 0)
+	light.castShadow = true
+	light.shadow.bias = -0.001
+	light.shadow.mapSize.width = 2048
+	light.shadow.mapSize.height = 2048
+	light.shadow.camera.near = 0.1
+	light.shadow.camera.far = 500.0
+	light.shadow.camera.near = 0.5
+	light.shadow.camera.far = 500.0
+	light.shadow.camera.left = 100
+	light.shadow.camera.right = -100
+	light.shadow.camera.top = 100
+	light.shadow.camera.bottom = -100
+	scene.add(light)
 
-	light = new THREE.AmbientLight(0x101010);
-	scene.add(light);
+	light = new THREE.AmbientLight(0x101010)
+	scene.add(light)
 
 	particles = new ParticleSystem({
 		parent: scene,
