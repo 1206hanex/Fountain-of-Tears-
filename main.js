338545a01b7ca53e8b67269bf57439421f0c2177
Hans Kristian Lomboy <111059193+1206hanex@users.noreply.github.com>
2023-05-25 22:38:29 +1200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { ParticleSystem } from './src/particleSystem'
+import { Fountain } from './src/fountain'
 
 let controls
 let scene
 let camera
 let renderer
-let particles
+let fountain
 
 function Init() {
 	scene = new THREE.Scene()
@@ -18,8 +18,6 @@ function Init() {
 	})
 	renderer.setPixelRatio(window.devicePixelRatio)
 	renderer.setSize(window.innerWidth, window.innerHeight)
-	renderer.shadowMap.enabled = true
-	renderer.shadowMap.type = THREE.PCFSoftShadowMap
 	document.body.appendChild(renderer.domElement)
 
 	camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1.0, 1000.0)
@@ -31,27 +29,10 @@ function Init() {
 	controls = new OrbitControls(camera, renderer.domElement)
 	controls.update()
 
-	let light = new THREE.DirectionalLight(0xFFFFFF, 1.0)
-	light.position.set(20, 100, 10)
-	light.target.position.set(0, 0, 0)
-	light.castShadow = true
-	light.shadow.bias = -0.001
-	light.shadow.mapSize.width = 2048
-	light.shadow.mapSize.height = 2048
-	light.shadow.camera.near = 0.1
-	light.shadow.camera.far = 500.0
-	light.shadow.camera.near = 0.5
-	light.shadow.camera.far = 500.0
-	light.shadow.camera.left = 100
-	light.shadow.camera.right = -100
-	light.shadow.camera.top = 100
-	light.shadow.camera.bottom = -100
-	scene.add(light)
+  const light = new THREE.AmbientLight(0x101010);
+	scene.add(light);
 
-	light = new THREE.AmbientLight(0x101010)
-	scene.add(light)
-
-	particles = new ParticleSystem({
+	fountain = new Fountain({
 		parent: scene,
 		camera: camera,
 	})
@@ -60,9 +41,9 @@ function Init() {
 }
 
 let prevAnimate = null
-function Animate(){
+function Animate() {
 	requestAnimationFrame((t) => {
-		if (prevAnimate === null){
+		if (prevAnimate === null) {
 			prevAnimate = t
 		}
 
@@ -74,10 +55,10 @@ function Animate(){
 	})
 }
 
-function step(timeElapsed){
+function step(timeElapsed) {
 	const timeElapsedS = timeElapsed * 0.001
 
-	particles.Step(timeElapsedS)
+	fountain.Step(timeElapsedS)
 }
 
 Init()
